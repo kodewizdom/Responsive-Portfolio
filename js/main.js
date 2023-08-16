43c01518fb4590504708bede5d31386ea04a4da1
Sajid Khan <mdskhan4774@gmail.com>
2023-08-16 13:10:57 +0530

    Added functionality for sending emails through contact section.

--- js/main.js
+++ js/main.js
@@ -40,7 +40,34 @@ window.addEventListener('scroll', shadowHeader)
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'), 
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #Form - publickey
+    emailjs.sendForm('service_iwj3k06','template_yo74nkv','#contact-form','QTkOpXKXa633yugNy')
+    .then(()=>{
+        // Show sent message
+        contactMessage.textContent = ' Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(()=>{
+            contactMessage.textContent=''
+        },5000)
+
+        // Clear input fields
+        contactForm.reset()
+
+      
+    },()=>{
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+} 
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
